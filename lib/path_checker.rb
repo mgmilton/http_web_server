9583d3da6c7a04786a653abc37462d700de09cb9
mgmilton <matt.milton@gmail.com>
2017-12-18 10:37:10 -0700

    first commit

---
+++ lib/path_checker.rb
@@ -0,0 +1,36 @@
+class PathChecker
+  attr_reader :path
+
+  def initialize(path)
+    @path = path
+  end
+
+  def root?
+    path == "/"
+  end
+
+  def hello?
+    path == "/hello"
+  end
+
+  def datetime?
+    path == "/datetime"
+   # Time.now.strftime('%I:%M%p on %A, %B %e, %Y')
+  end
+
+  def shutdown?
+    path == "shutdown"
+  end
+
+  def game?
+    path == "/game"
+  end
+
+  def start_game?
+    path == "/startgame"
+  end
+
+  def word_search?
+    path == "/word_search"
+  end
+end
